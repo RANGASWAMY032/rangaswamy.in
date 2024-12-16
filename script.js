20c288943d84745df68f36bde355a818b8a9e57a
RANGASWAMY032 <23022.cm.032@gmail.com>
2024-12-16 12:35:26 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Navbar Scroll Effect
-window.addEventListener('scroll', function() {
-    const navbar = document.querySelector('.navbar');
-    navbar.classList.toggle('scrolled', window.scrollY > 50);
-});
